87e67fea54a7bc0c9787ffb2897333c4ae01fac3
Nguyen_Duc_Tung_Lam <lam.nguyen@rikai.technology>
2025-09-12 18:53:07 +0700

    added common form for convinience

---
+++ SoftwareTestingExcercisesOfChapter4And5/DELETE/DELETE_CommonForm.xaml.cs
@@ -0,0 +1,15 @@
+﻿public partial class DELETE_CommonForm : Window, INotifyPropertyChanged
+{
+
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+    private void OnPropertyChanged (string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    public DELETE_CommonForm ()
+    {
+        InitializeComponent();
+    }
+}
