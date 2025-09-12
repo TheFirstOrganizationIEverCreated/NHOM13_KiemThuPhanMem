2babbfb15306473fb1c043a05abe8efe93a62ef6
Nguyen_Duc_Tung_Lam <lam.nguyen@rikai.technology>
2025-09-12 10:30:27 +0700

    added  SoftwareTestingExercisesOfChapter4And5  project

---
+++ SoftwareTestingExcercisesOfChapter4And5/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace SoftwareTestingExercisesOfChapter4And5
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ SoftwareTestingExcercisesOfChapter4And5/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿namespace SoftwareTestingExercisesOfChapter4And5;
+
+using System.Windows;
+
+public partial class MainWindow : Window
+{
+    public double X { private get; set; }
+
+    public MainWindow ()
+    {
+        InitializeComponent();
+    }
+
+    private double FunctionF ()
+    {
+        if (X >= 1)
+        {
+            return Math.Sqrt(Math.Pow(X, 2) + 1);
+        }
+        else if ((-1 < X) && (X < 1))
+        {
+            return 3 * X + 5;
+        }
+        else
+        {
+            return Math.Pow(X, 2) + 2 * X - 5;
+        }
+    }
+}
