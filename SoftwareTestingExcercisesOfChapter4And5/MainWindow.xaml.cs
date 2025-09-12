10dda7d7c0c62cdb4b34b5abfee889564ba3d055
Nguyen_Duc_Tung_Lam <lam.nguyen@rikai.technology>
2025-09-12 21:20:34 +0700

    finished exercise 4

--- SoftwareTestingExcercisesOfChapter4And5/MainWindow.xaml.cs
+++ SoftwareTestingExcercisesOfChapter4And5/MainWindow.xaml.cs
@@ -56,6 +56,9 @@ public partial class MainWindow : Window, INotifyPropertyChanged
             case Exercise.Exercise3:
                 (new Exercise3()).ShowDialog();
                 break;
+            case Exercise.Exercise4:
+                (new Exercise4()).ShowDialog();
+                break;
             default:
                 MessageBox.Show("Hiện chưa có bài này hoặc đã có lỗi.");
                 break;
