939cfd48b9db52ccd6b811f0bcf6e7c08e3aad93
Nguyen_Duc_Tung_Lam <lam.nguyen@rikai.technology>
2025-09-12 22:39:47 +0700

    working on exercise 6

---
+++ SoftwareTestingExcercisesOfChapter4And5/Exercise6.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace SoftwareTestingExercisesOfChapter4And5;
+using System.ComponentModel;
+using System.Windows;
+
+public partial class Exercise6 : Window, INotifyPropertyChanged
+{
+    public string? NString { private get; set; }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+    private void OnPropertyChanged (string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    public Exercise6 ()
+    {
+        InitializeComponent();
+    }
+}
