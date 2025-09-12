738d11470c30f455925f8ec3daa030c05dad6941
Nguyen_Duc_Tung_Lam <lam.nguyen@rikai.technology>
2025-09-12 21:32:45 +0700

    updated DELETE_CommonForm.xaml.cs

--- DELETE/DELETE_CommonForm.xaml.cs
+++ DELETE/DELETE_CommonForm.xaml.cs
@@ -1,6 +1,17 @@
 ﻿public partial class DELETE_CommonForm : Window, INotifyPropertyChanged
 {
+    public string? InputString { private get; set; }
 
+    private string? _output;
+    public string? OutputString
+    {
+        get => _output;
+        private set
+        {
+            _output = value;
+            OnPropertyChanged(nameof(OutputString));
+        }
+    }
 
     public event PropertyChangedEventHandler? PropertyChanged;
     private void OnPropertyChanged (string propertyName)
@@ -11,5 +22,34 @@
     public DELETE_CommonForm ()
     {
         InitializeComponent();
+
+        ResetOutput();
+    }
+
+    private void InputTextBoxChangedText (object sender, System.Windows.Controls.TextChangedEventArgs e)
+    {
+        if (!InputIsValid())
+        {
+            ResetOutput();
+            return;
+        }
+
+        var inputValue = .Parse(InputString);
+        OutputString = GetOutput(inputValue);
+    }
+    private bool InputIsValid ()
+    {
+        return .TryParse(InputString, out var inputValue) &&  && ;
+    }
+    private string GetOutput (double n)
+    {
+        double numerator = 0;
+        for (uint integer = 1; integer <= n; integer++)
+        {
+            numerator += integer;
+        }
+
+        var outputValue = numerator / n;
+        return outputValue.ToString();
     }
 }
