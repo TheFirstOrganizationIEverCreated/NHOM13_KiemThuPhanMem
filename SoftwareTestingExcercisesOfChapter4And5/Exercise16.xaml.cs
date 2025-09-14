005085fa004449a4b9a7c242b9ea373ff1c10a8d
thanh <thanh@THANHPX-FX505DD>
2025-09-14 21:04:10 +0700

    Hoàn thành bài 16

--- SoftwareTestingExcercisesOfChapter4And5/Exercise16.xaml.cs
+++ SoftwareTestingExcercisesOfChapter4And5/Exercise16.xaml.cs
@@ -58,4 +58,79 @@ public partial class Exercise16 : Window, INotifyPropertyChanged
     {
         Notification = string.Empty;
     }
+
+    private readonly List<int> _numbers = new();
+
+    public string? AString { private get; set; }
+
+    private void ClickedConfirmNButtonEventHandler (object sender, RoutedEventArgs e)
+    {
+        if (!NIsValid())
+        {
+            return;
+        }
+
+        confirmNSection.Visibility = Visibility.Collapsed;
+        ClickedConfirmNButton.Visibility = Visibility.Collapsed;
+        confirmUSection.Visibility = Visibility.Visible;
+        uTextBox.Text = "{}";
+    }
+
+    // --- Kiểm tra A ---
+    private bool AIsValid ()
+    {
+        return int.TryParse(AString, out _);
+    }
+
+    private void TextBoxAChangedTextEventHandler (object sender, System.Windows.Controls.TextChangedEventArgs e)
+    {
+        const string aIsInvalidNotification = "a phải thuộc Z";
+        if (!AIsValid())
+        {
+            aNotificationTextBlock.Text = aIsInvalidNotification;
+        }
+        else
+        {
+            aNotificationTextBlock.Text = string.Empty;
+        }
+    }
+
+    private void ClickedConfirmAButtonEventHandler (object sender, RoutedEventArgs e)
+    {
+        if (!AIsValid())
+        {
+            return;
+        }
+
+        var a = int.Parse(AString);
+        _numbers.Add(a);
+
+        var n = int.Parse(NString);
+        if (_numbers.Count != n)
+        {
+            UpdateUTextBox();
+        }
+        else
+        {
+            ShowMaxElementAndPosition();
+        }
+    }
+
+    private void UpdateUTextBox ()
+    {
+        uTextBox.Text = "{" + string.Join(" ; ", _numbers) + "}";
+    }
+
+    private void ShowMaxElementAndPosition ()
+    {
+        confirmUSection.Visibility = Visibility.Collapsed;
+
+        var max = _numbers.Max();
+        var index = _numbers.IndexOf(max) + 1; // vị trí tính từ 1
+
+        maxElementTextBlock.Text = $"Phần tử lớn nhất = {max}";
+        positionTextBlock.Text = $"Vị trí trong dãy = {index}";
+
+        resultSection.Visibility = Visibility.Visible;
+    }
 }
