7ff9c1f5139d3c5b8c41991498e2176e97981bbe
Nguyen_Duc_Tung_Lam <lam.nguyen@rikai.technology>
2025-09-16 01:22:21 +0700

    added Common class

---
+++ SoftwareTestingExcercisesOfChapter4And5/Common/Common.cs
@@ -0,0 +1,9 @@
+﻿namespace SoftwareTestingExercisesOfChapter4And5.Common;
+
+internal class Common
+{
+    public static string CollectionString<CollectionType> (CollectionType collection) where CollectionType : System.Collections.IEnumerable
+    {
+        return "{" + string.Join(" ; ", collection) + "}";
+    }
+}
