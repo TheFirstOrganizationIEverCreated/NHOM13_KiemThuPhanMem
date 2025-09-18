4d9db7921434c1138fb9541619d88f6d0a2e386d
NguyenDucTungLam <tenlanguyenductunglam>
2025-09-18 21:12:30 +0700

    start exercise 27

---
+++ SoftwareTestingExcercisesOfChapter4And5/Exercise27.xaml.cs
@@ -0,0 +1,34 @@
+﻿namespace SoftwareTestingExercisesOfChapter4And5;
+
+using System.ComponentModel;
+using System.Windows;
+
+public partial class Exercise27 : Window, INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler? PropertyChanged;
+    private void OnPropertyChanged (string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    private string? _notification;
+    public string? Notification
+    {
+        get => _notification;
+        private set
+        {
+            if (_notification == value)
+            {
+                return;
+            }
+
+            _notification = value;
+            OnPropertyChanged(nameof(Notification));
+        }
+    }
+
+    public Exercise27 ()
+    {
+        InitializeComponent();
+    }
+}
--- SoftwareTestingExcercisesOfChapter4And5/MainWindow.xaml.cs
+++ SoftwareTestingExcercisesOfChapter4And5/MainWindow.xaml.cs
@@ -111,6 +111,9 @@ public partial class MainWindow : Window, INotifyPropertyChanged
             case Exercise.Exercise26:
                 (new Exercise26()).ShowDialog();
                 break;
+            case Exercise.Exercise27:
+                (new Exercise27()).ShowDialog();
+                break;
             default:
                 MessageBox.Show("Hiện chưa có bài này hoặc đã có lỗi.");
                 break;
